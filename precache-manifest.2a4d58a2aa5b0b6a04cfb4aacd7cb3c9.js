5e03f96e1ebb0aeb5efb5ad3c17c32a58e7685c1
Digvijay Singh <digvijayrn786@gmail.com>
2019-04-21 22:20:47 +0530

    Updates

---
+++ precache-manifest.2a4d58a2aa5b0b6a04cfb4aacd7cb3c9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c17e7a5d3d1b7d702bc8c640a36f68c6",
+    "url": "/thundersky/static/media/clearRainy.c17e7a5d.svg"
+  },
+  {
+    "revision": "184092577b9b274e61a1",
+    "url": "/thundersky/static/css/main.d0a00660.chunk.css"
+  },
+  {
+    "revision": "e0bcaee2a1e81deaa2c0",
+    "url": "/thundersky/static/js/runtime~main.9d40389a.js"
+  },
+  {
+    "revision": "38cc15545184c2955cbc",
+    "url": "/thundersky/static/js/2.2d7880b8.chunk.js"
+  },
+  {
+    "revision": "d03f455906deba9bab06289979f14bb1",
+    "url": "/thundersky/static/media/evening.d03f4559.png"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/thundersky/static/media/clearDay.7f93fbdb.svg"
+  },
+  {
+    "revision": "a3f3f38d2c17971938ebd891fb6bbd0c",
+    "url": "/thundersky/static/media/clearNight.a3f3f38d.svg"
+  },
+  {
+    "revision": "184092577b9b274e61a1",
+    "url": "/thundersky/static/js/main.0acea956.chunk.js"
+  },
+  {
+    "revision": "109368f632c5dafab6cbf47ec386f8d0",
+    "url": "/thundersky/static/media/cloudyDay.109368f6.svg"
+  },
+  {
+    "revision": "bf91ac0f97cf729862dfaeb8fc25e037",
+    "url": "/thundersky/static/media/cloudyNight.bf91ac0f.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/thundersky/static/media/rain.c915395f.svg"
+  },
+  {
+    "revision": "67d63a14dbbef1d431f1851da197e0ae",
+    "url": "/thundersky/static/media/snowy.67d63a14.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/thundersky/static/media/thunder.7142c78b.svg"
+  },
+  {
+    "revision": "454f999c9f08bcccd6c71bc7cb130548",
+    "url": "/thundersky/index.html"
+  }
+];
--- src/Components/CurrentCity.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function currentCity(props) {
-  return <div style={{textAlign: "center", padding: "20px"}}><h1>Weather of {props.city}</h1></div>;
-}
